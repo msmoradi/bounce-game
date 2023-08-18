36b3334465e6554c59bd7c402cdb305a72a29587
Saeed <saeed@Saeeds-MacBook-Pro-3.local>
2023-08-18 20:58:57 +0330

    first commit

---
+++ app/src/main/java/com/saeed/androidtaks/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.saeed.androidtaks
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.saeed.androidtaks.ui.theme.AndroidTaksTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidTaksTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
